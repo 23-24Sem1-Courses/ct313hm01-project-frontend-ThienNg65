de347d9863492c60530883a690e42112d1173861
Thien Nguyen <nvtt1510@gmail.com>
2023-10-31 16:15:47 +0700

    category add views

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import router from './router'
 
 createApp(App).use(router).mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import { createWebHistory, createRouter } from 'vue-router';
+import Home from '../views/HomeView.vue';
+import About from '../views/AboutView.vue';
+import AddCategory from '../views/Category/AddCategory.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/AddCategory',
+    name: 'AddCategory',
+    component: AddCategory
   }
 ]
 
+// const router = new VueRouter({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes
+// })
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
