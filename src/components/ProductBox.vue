0c10de8de618b914d73dccfa668f1a4b5db28129
Thien Nguyen <nvtt1510@gmail.com>
2023-11-01 10:37:27 +0700

    Feature Product

--- src/components/ProductBox.vue
+++ src/components/ProductBox.vue
@@ -19,8 +19,7 @@
 <script>
 export default {
   name: 'ProductBox',
-  props: ['product'],
-  methods: {}
+  props: ['product']
 };
 </script>
 <style scoped>
--- src/views/Product/Product.vue
+++ src/views/Product/Product.vue
@@ -3,18 +3,18 @@
   <div class="container">
     <div class="row">
       <div class="col-12 text-center">
+        <br />
         <h4>Our Products</h4>
         <router-link :to="{ name: 'AddProduct' }" style="float: right">
           <button class="btn">Add Product</button>
         </router-link>
       </div>
     </div>
-    <div class="row">
-      <!--            display all the products in productbox component-->
+    <div class="row mb-5">
       <div
-        v-for="product of products"
+        v-for="product in product"
         :key="product.id"
-        class="col-md-6 col-xl-4 col-12 pt-3 d-flex"
+        class="col-md-6 col-xl-4 col-12 pt-3 justify-content-around d-flex"
       >
         <ProductBox :product="product" />
       </div>
@@ -25,6 +25,27 @@
 import ProductBox from '../../components/ProductBox.vue';
 export default {
   components: { ProductBox },
-  props: ['products']
+  props: ['categories', 'products'],
+  data() {
+    return {
+      id: null,
+      category: {},
+      msg: '',
+      product: {}
+    };
+  },
+  mounted() {
+    this.id = this.$route.params.id;
+    this.category = this.categories.find((category) => category.id == this.id);
+    this.product = this.products.filter((product) => product.categoryId == this.id);
+
+    if (this.product.length == 0) {
+      this.msg = 'no products found';
+    } else if (this.product.length == 1) {
+      this.msg = 'Only 1 product found';
+    } else {
+      this.msg = this.product.length + ' products found';
+    }
+  }
 };
 </script>
