2909a9a8c8b77a50eff902f5b8229674ce47e9a8
Thien Nguyen <nvtt1510@gmail.com>
2023-11-16 22:07:56 +0700

    Order and Order Details and fakeData for testing

--- src/views/Orders/MyOrder.vue
+++ src/views/Orders/MyOrder.vue
@@ -1,59 +1,65 @@
 <template>
   <div class="my-orders">
     <h3>My Orders</h3>
-    <div v-if="orderList.length > 0">
-      <div v-for="order in orderList" :key="order.id" class="order-card mt-4">
+    <div v-if="orders.length > 0">
+      <div v-for="order in orders" :key="order.id" class="order-card mt-4">
         <!-- <router-link :to="{ name: 'OrderDetails' }"> -->
         <h2>Order #{{ order.id }}</h2>
         <!-- </router-link> -->
         <p class="order-info">Created Date: {{ order.createdDate }}</p>
-        <p class="order-info">Total Price: ${{ order.price }}</p>
+        <p class="order-info">Total Price: ${{ order.totalPrice }}</p>
         <ul class="order-items">
           orders
           <li v-for="item in order.items" :key="item.id">
             {{ item.quantity }} x {{ item.product.name }} - ${{ item.price }}
           </li>
         </ul>
+        <h6 class="card-title">
+          <router-link v-bind:to="'/order/' + order.id">Order No : {{ order.id }}</router-link>
+        </h6>
       </div>
     </div>
-    <div v-if="orderList.length === 0" class="no-orders">
+    <div v-if="orders.length === 0" class="no-orders">
       <p>No orders available.</p>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import { myOrdersData } from './TestItemData.js';
+// import axios from 'axios';
 export default {
   name: 'MyOrder',
   // props: ['baseURL'],
   data() {
     return {
-      orderList: [],
+      orders: myOrdersData,
+      // orderList: [],
       token: null
       // loading: true
     };
   },
 
   // FUNCTION RETRIVE 1
-  // mounted() {
-  //   // Fetch orders data from your API endpoint
-  //   this.fetchOrders();
-  // },
-  // methods: {
-  //   fetchOrders() {
-  //     // Replace this with your actual API endpoint to fetch orders data
-  //     // For simplicity, I'm using a fake API endpoint here
-  //     fetch('api/order')
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         this.orders = data;
-  //       })
-  //       .catch((error) => {
-  //         console.error('Error fetching orders:', error);
-  //       });
-  //   }
-  // }
+  mounted() {
+    // Fetch orders data from your API endpoint
+    this.fetchOrders();
+  },
+  methods: {
+    fetchOrders() {
+      // Replace this with your actual API endpoint to fetch orders data
+      // For simplicity, I'm using a fake API endpoint here
+      // fetch('api/order')
+      //   .then((response) => response.json())
+      //   .then((data) => {
+      //     this.orders = data;
+      //   })
+      //   .catch((error) => {
+      //     console.error('Error fetching orders:', error);
+      //   });
+      this.orders = myOrdersData;
+    }
+  }
 
   // FUNCTION RETRIVE 2
   // async created() {
@@ -72,38 +78,38 @@ export default {
   //   }
 
   // FUNCTION RETRIEVE 3
-  methods: {
-    // list of order histories
-    listOrders() {
-      axios.get(`api/order/?token=${this.token}`).then(
-        (response) => {
-          if (response.status == 200) {
-            this.orders = response.data;
-            // for each order populate orderList
-            this.orders.forEach((order) => {
-              this.orderList.push({
-                id: order.id,
-                totalCost: order.totalPrice,
-                // get short date
-                orderdate: order.createdDate.substring(0, 10),
-                // get image of the first orderItem of the order
-                imageURL: order.orderItems[0].product.imageURL,
-                // get total items
-                totalItems: order.orderItems.length
-              });
-            });
-          }
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-    }
-  },
-  mounted() {
-    this.token = localStorage.getItem('token');
-    this.listOrders();
-  }
+  // methods: {
+  //   // list of order histories
+  //   listOrders() {
+  //     axios.get(`api/order/?token=${this.token}`).then(
+  //       (response) => {
+  //         if (response.status == 200) {
+  //           this.orders = response.data;
+  //           // for each order populate orderList
+  //           this.orders.forEach((order) => {
+  //             this.orderList.push({
+  //               id: order.id,
+  //               totalCost: order.totalPrice,
+  //               // get short date
+  //               orderdate: order.createdDate.substring(0, 10),
+  //               // get image of the first orderItem of the order
+  //               imageURL: order.orderItems[0].product.imageURL,
+  //               // get total items
+  //               totalItems: order.orderItems.length
+  //             });
+  //           });
+  //         }
+  //       },
+  //       (error) => {
+  //         console.log(error);
+  //       }
+  //     );
+  //   }
+  // },
+  // mounted() {
+  //   this.token = localStorage.getItem('token');
+  //   this.listOrders();
+  // }
   // }
 };
 </script>
